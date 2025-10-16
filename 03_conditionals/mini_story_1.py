0adfa962ab25bbe90a0b248978c16761827230f1
Mohammad Shahid <www.shahid6516@gmail.com>
2025-10-17 00:46:14 +0530

    snack_suggestion system

--- 03_conditionals/mini_story_1.py
+++ 03_conditionals/mini_story_1.py
@@ -8,3 +8,6 @@ kettle_boiled = True
 
 if kettle_boiled:
     print("Kettle Done! time to make chai")
+
+
+
---
+++ 03_conditionals/snack_suggestion.py
@@ -0,0 +1,8 @@
+
+snack = input("Enter your preferd snack: ")
+
+if snack == "cookies" or snack == "samosa":
+    print(f"Great choise! we will serve you {snack}")
+else:
+    print("Sorry we only serve cookies or samosa")
+
