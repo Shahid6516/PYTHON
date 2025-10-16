fafa0865884bca7c584864c5632be5dc18639e24
Mohammad Shahid <www.shahid6516@gmail.com>
2025-10-16 12:24:46 +0530

    Python

---
+++ 02_datatypes/chapter_1.py
@@ -0,0 +1,7 @@
+# Integers are immutable — reassigning creates a new object with a different id.
+
+
+sugar_amount=2
+print(f"initial value:{sugar_amount}")
+sugar_amount=12
+print(f"second initial value:{sugar_amount}")
