915ff478a568e263ff325a1ce716fae4711f3336
Mohammad Shahid <www.shahid6516@gmail.com>
2025-10-17 01:32:03 +0530

    chai price calculator

---
+++ 03_conditionals/chai_price_calculator.py
@@ -0,0 +1,10 @@
+cup = input("Choose your cup size (small/medium/large): ").lower()
+
+if cup == "small":
+    print("Your tea price is 10₹")
+elif cup == "medium":
+    print("Your tea price is 20₹")
+elif cup == "large":
+    print("Your tea price is 30₹")
+else:
+    print("Invalid cup size")
--- 03_conditionals/snack_suggestion.py
+++ 03_conditionals/snack_suggestion.py
@@ -1,8 +1,18 @@
 
-snack = input("Enter your preferd snack: ")
+# snack = input("Enter your preferd snack: ")
 
-if snack == "cookies" or snack == "samosa":
-    print(f"Great choise! we will serve you {snack}")
-else:
-    print("Sorry we only serve cookies or samosa")
+# if snack == "cookies" or snack == "samosa":
+#     print(f"Great choise! we will serve you {snack}")
+# else:
+#     print("Sorry we only serve cookies or samosa")
+
+
+
+cup_sizes = input("Please select your Size: ")
+if cup_sizes == "small" or "medium" or "large":
+    
+
+   
+
+
 
